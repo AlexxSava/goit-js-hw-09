e1f9825207f92a0442a5bb77a8bd0169b01d846d
Alex Savelev <savaqwerty2@gmail.com>
2025-04-04 20:54:54 +0200

    update hw

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,17 +1,17 @@
 const formEl = document.querySelector('.feedback-form');
-const emailInputEl = document.querySelector('.input-email');
-const messageInputEl = document.querySelector('.input-message');
+const emailInputEl = formEl.elements.email; 
+const messageInputEl = formEl.elements.message;
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const localData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+const localData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 if (localData) {
   emailInputEl.value = localData.email || '';
   messageInputEl.value = localData.message || '';
 }
 
-const getTrimmedValue = value => value.value.trim();
+const getTrimmedValue = element => element.value.trim();
 
 const handleFormInput = () => {
   const currentData = {
@@ -28,14 +28,15 @@ const handleFormSubmit = event => {
   const email = getTrimmedValue(emailInputEl);
   const message = getTrimmedValue(messageInputEl);
 
-  if (email && message) {
-    console.log({ email, message });
-
-    localStorage.removeItem(STORAGE_KEY);
-
-    emailInputEl.value = '';
-    messageInputEl.value = '';
+  if (!email || !message) {
+    alert('Fill please all fields');
+    return; 
   }
+
+  console.log({ email, message });
+
+  localStorage.removeItem(STORAGE_KEY);
+  formEl.reset();
 };
 
 formEl.addEventListener('input', handleFormInput);
